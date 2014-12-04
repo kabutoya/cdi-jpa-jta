2dc19258ad1a8aac3eb449c0250fa204e337bb9d
Masamitsu Namioka <masamitsunamioka@gmail.com>
2014-12-04 11:54:16 +0900

    Initial commit

---
+++ experimental/src/main/java/com/github/namioka/cdi_jpa_jta/App.java
@@ -0,0 +1,13 @@
+package com.github.namioka.cdi_jpa_jta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
